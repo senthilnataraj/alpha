e64d0990264f7e9069ca5e6f9255fc4a950e744a
senthilnataraj <37336427+senthilnataraj@users.noreply.github.com>
2018-03-21 11:51:36 +0530

    Add files via upload

---
+++ README.md/alhabet.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      loges
+#
+# Created:     20/03/2018
+# Copyright:   (c) loges 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    ch=input()
+    if ch.isalpha:
+        print("alphabet")
+    else:
+        print("not alphabet")
+
+if __name__ == '__main__':
+    main()
